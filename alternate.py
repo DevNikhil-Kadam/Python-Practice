d2f60de55755b455895b1f36f859221e191533a8
DevNikhil-Kadam <nikhil.kadam1823@gmail.com>
2026-07-07 21:41:57 +0530

    day2

---
+++ alternate.py
@@ -0,0 +1,11 @@
+arr = [1,2,3,4,5]
+
+output = []
+
+for i in range(len(arr)):
+    if i % 2 == 0:
+        output.append(arr[i])
+    else:
+        continue
+
+print(output)
---
+++ oddEven.py
@@ -0,0 +1,10 @@
+arr = [1,2,3,4,5]
+
+odd = 0
+even = 0
+for i in range(len(arr)):
+    if arr[i] % 2 == 0:
+        even += 1
+    else:
+        odd += 1
+print(even, odd)
